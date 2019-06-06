dceff072534522b5c93d4bf17839b7026bf30fe7
nkong009 <kong_1989>
2019-06-06 16:22:53 +0800

    update login test

--- SampleProject/POMProject/Tests/Login.py
+++ SampleProject/POMProject/Tests/Login.py
@@ -4,8 +4,12 @@ import time
 import HtmlTestRunner
 from SampleProject.POMProject.Pages.LoginPage import LoginPage
 from SampleProject.POMProject.Pages.Homepage import Homepage
-from Common.CommonMethod import read_excel
+#from Common.CommonMethod import read_excel
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "...", "..."))
 
+#print(sys.path)
 class LoginTests(unittest.TestCase):
     @classmethod
     def setUp(cls):
@@ -16,29 +20,42 @@ class LoginTests(unittest.TestCase):
     def test_01_ValidUserName(self):
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
         time.sleep(3)
-        valid_data = read_excel(r'C:\Users\nkong009\PycharmProjects\NancyProject\Data\TestExcel.xlsx',1)
         login = LoginPage(self.driver)
         home = Homepage(self.driver)
-        login.enter_username(valid_data[0])
-        login.enter_password(valid_data[1])
+        login.enter_username("Admin")
+        login.enter_password("admin123")
         login.click_login()
         self.driver.get_screenshot_as_file("C:\TestScreenshots\ABC.png")
         time.sleep(5)
         home.click_welcome()
         home.click_logout()
 
-    def test_02_EmptyUserName(self):
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        time.sleep(3)
-        for i in range(2,6):
-            invalid_data = read_excel(r'C:\Users\nkong009\PycharmProjects\NancyProject\Data\TestExcel.xlsx', i)
-            login = LoginPage(self.driver)
-            login.enter_username(invalid_data [0])
-            login.enter_password(invalid_data [1])
-            login.click_login()
-            error_msg = login.check_invalid_username_msg()
-            self.assertEqual(error_msg.strip(), invalid_data[2].strip())
-        time.sleep(3)
+    # def test_01_ValidUserName(self):
+    #     self.driver.get("https://opensource-demo.orangehrmlive.com/")
+    #     time.sleep(3)
+    #     valid_data = read_excel(r'C:\Users\nkong009\PycharmProjects\NancyProject\Data\TestExcel.xlsx',1)
+    #     login = LoginPage(self.driver)
+    #     home = Homepage(self.driver)
+    #     login.enter_username(valid_data[0])
+    #     login.enter_password(valid_data[1])
+    #     login.click_login()
+    #     self.driver.get_screenshot_as_file("C:\TestScreenshots\ABC.png")
+    #     time.sleep(5)
+    #     home.click_welcome()
+    #     home.click_logout()
+
+    # def test_02_EmptyUserName(self):
+    #     self.driver.get("https://opensource-demo.orangehrmlive.com/")
+    #     time.sleep(3)
+    #     for i in range(2,6):
+    #         invalid_data = read_excel(r'C:\Users\nkong009\PycharmProjects\NancyProject\Data\TestExcel.xlsx', i)
+    #         login = LoginPage(self.driver)
+    #         login.enter_username(invalid_data [0])
+    #         login.enter_password(invalid_data [1])
+    #         login.click_login()
+    #         error_msg = login.check_invalid_username_msg()
+    #         self.assertEqual(error_msg.strip(), invalid_data[2].strip())
+    #     time.sleep(3)
 
     @classmethod
     def tearDown(cls):
